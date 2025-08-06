84262f9035a6bb3a8facf15b4a93e61972925041
Sakib Nihal Arnab <sakibnihalarnab@gmail.com>
2025-08-06 18:11:03 +0600

    OK

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'RCInventory' );
+define( 'DB_NAME', 'headphone' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
